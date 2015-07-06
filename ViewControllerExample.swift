d6b3f56a1b66705e7e46c6aab594bdb6358e6bf2
Ruslan Ardashev <ardashev120101@gmail.com>
2015-07-06 07:12:58 -0500

    examples

--- ViewControllerExample.swift
+++ ViewControllerExample.swift
@@ -6,27 +6,29 @@ Licensed Materials - Property of IBM
 import UIKit
 
 class ViewControllerExample: UIViewController {
-
-    @IBOutlet weak var milRatingCollectionView: MILRatingCollectionView!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        milRatingCollectionView.circularView.backgroundColor = UIColor(red: 0.0/255.0, green: 178.0/255.0, blue: 239.0/255.0, alpha: 1.0)
-        milRatingCollectionView.numberRange = NSMakeRange(1, 11) // Default range is 0 to 10, only change if necessary
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        // Set the starting position for the collectionView before view is visible
-        self.milRatingCollectionView.scrollToItemAtIndexPath(milRatingCollectionView.selectedIndexPath!, atScrollPosition: .CenteredHorizontally, animated: false)
+        let currFrame = self.view.frame
+        let bobsFrame = CGRect(x: 0.0, y: 40.0, width: currFrame.width, height: currFrame.height/5)
+        
+        let bob = MILUI.RatingCollectionView(frame: bobsFrame)
+        
+        var bobConstants = MILUI.RatingCollectionView.Constants()
+        bobConstants.fontAnimated = true
+        bobConstants.backgroundColor = UIColor.blueColor()
+        bobConstants.circleBackgroundColor = UIColor.cyanColor()
+        bobConstants.numberRange = NSMakeRange(4, 21)
+        
+        bob.constants = bobConstants
+        
+        self.view.addSubview(bob)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
 
 }
 
